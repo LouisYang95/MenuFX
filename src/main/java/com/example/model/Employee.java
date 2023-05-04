e1dc5f77bc444e600763aa80e4f9f631f0ed4bed
Adje-Abed <abedadje@gmail.com>
2023-05-05 01:03:10 +0200

    employee

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,16 +1,39 @@
 package com.example.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
     private String name;
     private String position;
     private float hoursWorked;
+    private List<Employee> employees;
+    private Object GestionEmployee;
+    private int id;
 
-    public Employee(String name, String position, float hoursWorked) {
+    public Employee(String name, String position) {
         this.name = name;
         this.position = position;
         this.hoursWorked = hoursWorked;
+        this.employees = new ArrayList<>();
+        this.GestionEmployee = GestionEmployee;
+        this.id = id ;
     }
 
+    public Employee() {
+
+    }
+
+    public static void add(Employee employee1) {
+
+    }
+
+    public int getid() {
+        return id;
+    }
+    public void setid(int id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
@@ -39,6 +62,24 @@ public class Employee {
         this.hoursWorked += hoursWorked;
     }
 
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        this.employees.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        this.employees.remove(employee);
+    }
 
 
 }
+
+
+
+
+
+
+
